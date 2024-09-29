bd10920d1ca327d8fcfd45c1ed4d742f3873301b
Tanmay Sarkar <tanmay.sarkar@nirmata.com>
2024-09-29 19:32:23 +0530

    feat: baseUrl update

--- frontend/src/lib/network.ts
+++ frontend/src/lib/network.ts
@@ -1,4 +1,4 @@
-export const baseUrl =
-  process.env.NODE_ENV === "development"
-    ? "http://localhost:8080/v1"
-    : process.env.SERVER_URL;
+export const baseUrl = "https://tsmk-dnd-backend.vercel.app/v1";
+  // process.env.NODE_ENV === "development"
+  //   ? "http://localhost:8080/v1"
+  //   : process.env.SERVER_URL;
